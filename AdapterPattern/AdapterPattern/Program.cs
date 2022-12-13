76a43e43de796d321eee75d11b23f69fcf9f1937
haozhang <haozhan@microsoft.com>
2022-12-13 20:38:52 +0800

    adapter pattern

---
+++ AdapterPattern/AdapterPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿
+// Adapter Pattern
+
+using AdapterPattern.Adapter;
+using AdapterPattern.Ducks;
+using AdapterPattern.Turkeys;
+
+namespace AdapterPattern
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            IDuck duck = new MallarDuck();
+            duck.Fly();
+            duck.Quack();
+
+            ITurkey turkey = new WildTurkey();
+            TurkeyAdapter adapter = new TurkeyAdapter(turkey);
+            adapter.Fly();
+            adapter.Quack();
+        }
+    }
+}
